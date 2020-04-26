f7dee270c615e8f46520bc7665a61be23f55e889
Cameron Steele <cam.steeleis@gmail.com>
2020-04-25 21:19:53 -0500

    Added .classpath, .project and .settings to .gitignore

--- src/main/java/io/servertap/api/v1/ServerApi.java
+++ src/main/java/io/servertap/api/v1/ServerApi.java
@@ -20,7 +20,6 @@ import java.math.BigDecimal;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
