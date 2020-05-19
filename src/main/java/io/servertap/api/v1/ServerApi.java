62a848fc2f30930732f4cface4056c2eaee8aae2
Will Warren <willdavidwarren@gmail.com>
2020-05-18 22:46:32 -0400

    Added save methods, changed to uuids (#20)
    * Added save methods, changed to uuids
    
    * Added OAS annotations
    
    * Fixed up import shenanigans

--- src/main/java/io/servertap/api/v1/ServerApi.java
+++ src/main/java/io/servertap/api/v1/ServerApi.java
@@ -4,16 +4,11 @@ import com.google.gson.Gson;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import io.javalin.plugin.openapi.annotations.*;
-import io.servertap.api.v1.models.Plugin;
-import io.servertap.api.v1.models.Server;
-import io.servertap.api.v1.models.ServerBan;
-import io.servertap.api.v1.models.ServerHealth;
-import io.servertap.api.v1.models.Whitelist;
-import io.servertap.api.v1.models.World;
+import io.servertap.api.v1.models.*;
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-
+import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -22,10 +17,7 @@ import java.lang.management.ManagementFactory;
 import java.math.BigDecimal;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class ServerApi {
@@ -33,24 +25,24 @@ public class ServerApi {
     private static final Logger log = Bukkit.getLogger();
 
     @OpenApi(
-        path = "/v1/ping",
-        summary = "pong!",
-        tags = {"Server"},
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
-        }
+            path = "/v1/ping",
+            summary = "pong!",
+            tags = {"Server"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
+            }
     )
     public static void ping(Context ctx) {
         ctx.json("pong");
     }
 
     @OpenApi(
-        path = "/v1/server",
-        summary = "Get information about the server",
-        tags = {"Server"},
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(from = Server.class))
-        }
+            path = "/v1/server",
+            summary = "Get information about the server",
+            tags = {"Server"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(from = Server.class))
+            }
     )
     public static void serverGet(Context ctx) {
         Server server = new Server();
@@ -122,16 +114,80 @@ public class ServerApi {
     }
 
     @OpenApi(
-        path = "/v1/broadcast",
-        method = HttpMethod.POST,
-        summary = "Send broadcast visible to those currently online.",
-        tags = {"Server"},
-        formParams = {
-            @OpenApiFormParam(name = "message", type = String.class)
-        },
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
+            path = "/v1/worlds/save",
+            summary = "Triggers a world save of all worlds",
+            method = HttpMethod.POST,
+            tags = {"Server"},
+            responses = {
+                    @OpenApiResponse(status = "200")
+            }
+    )
+    public static void saveAllWorlds(Context ctx) {
+        org.bukkit.Server bukkitServer = Bukkit.getServer();
+
+        Plugin pluginInstance = bukkitServer.getPluginManager().getPlugin("ServerTap");
+
+        if (pluginInstance != null) {
+            // Run the saves on the main thread, can't use sync methods from here otherwise
+            bukkitServer.getScheduler().scheduleSyncDelayedTask(pluginInstance, () -> {
+                for (org.bukkit.World world : Bukkit.getWorlds()) {
+                    try {
+                        world.save();
+                    } catch (Exception e) {
+                        // Just warn about the issue
+                        log.warning(String.format("Couldn't save World %s %s", world.getName(), e.getMessage()));
+                    }
+                }
+            });
         }
+
+        ctx.json("success");
+    }
+
+    @OpenApi(
+            path = "/v1/worlds/:uuid/save",
+            summary = "Triggers a world save",
+            method = HttpMethod.POST,
+            tags = {"Server"},
+            responses = {
+                    @OpenApiResponse(status = "200")
+            }
+    )
+    public static void saveWorld(Context ctx) {
+        org.bukkit.Server bukkitServer = Bukkit.getServer();
+        org.bukkit.World world = bukkitServer.getWorld(UUID.fromString(ctx.pathParam(":uuid")));
+
+        if (world != null) {
+            Plugin pluginInstance = bukkitServer.getPluginManager().getPlugin("ServerTap");
+
+            if (pluginInstance != null) {
+                // Run the saves on the main thread, can't use sync methods from here otherwise
+                bukkitServer.getScheduler().scheduleSyncDelayedTask(pluginInstance, () -> {
+
+                    try {
+                        world.save();
+                    } catch (Exception e) {
+                        // Just warn about the issue
+                        log.warning(String.format("Couldn't save World %s %s", world.getName(), e.getMessage()));
+                    }
+                });
+            }
+        }
+
+        ctx.json("success");
+    }
+
+    @OpenApi(
+            path = "/v1/broadcast",
+            method = HttpMethod.POST,
+            summary = "Send broadcast visible to those currently online.",
+            tags = {"Server"},
+            formParams = {
+                    @OpenApiFormParam(name = "message", type = String.class)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
+            }
     )
     public static void broadcastPost(Context ctx) {
         Bukkit.broadcastMessage(ctx.formParam("message"));
@@ -139,12 +195,12 @@ public class ServerApi {
     }
 
     @OpenApi(
-        path = "/v1/worlds",
-        summary = "Get information about all worlds",
-        tags = {"Server"},
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(from = World.class, isArray = true))
-        }
+            path = "/v1/worlds",
+            summary = "Get information about all worlds",
+            tags = {"Server"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(from = World.class, isArray = true))
+            }
     )
     public static void worldsGet(Context ctx) {
         List<World> worlds = new ArrayList<>();
@@ -154,18 +210,19 @@ public class ServerApi {
     }
 
     @OpenApi(
-        path = "/v1/worlds/:world",
-        summary = "Get information about a specific world",
-        tags = {"Server"},
-        pathParams = {
-            @OpenApiParam(name = "world", description = "The name of the world")
-        },
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(from = World.class))
-        }
+            path = "/v1/worlds/:world",
+            summary = "Get information about a specific world",
+            tags = {"Server"},
+            pathParams = {
+                    @OpenApiParam(name = "world", description = "The name of the world")
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(from = World.class))
+            }
     )
     public static void worldGet(Context ctx) {
-        org.bukkit.World bukkitWorld = Bukkit.getServer().getWorld(ctx.pathParam("world"));
+        UUID worldUuid = UUID.fromString(ctx.pathParam(":uuid"));
+        org.bukkit.World bukkitWorld = Bukkit.getServer().getWorld(worldUuid);
 
         // 404 if no world found
         if (bukkitWorld == null) throw new NotFoundResponse();
@@ -177,6 +234,7 @@ public class ServerApi {
         World world = new World();
 
         world.setName(bukkitWorld.getName());
+        world.setUuid(bukkitWorld.getUID().toString());
 
         // TODO: The Enum for Environment makes this annoying to get
         switch (bukkitWorld.getEnvironment()) {
@@ -223,25 +281,25 @@ public class ServerApi {
         return world;
     }
 
-    private static Set<Whitelist> getWhitelist(){
+    private static Set<Whitelist> getWhitelist() {
         Set<Whitelist> whitelist = new HashSet<Whitelist>();
-        Bukkit.getServer().getWhitelistedPlayers().forEach( (OfflinePlayer player) -> {
+        Bukkit.getServer().getWhitelistedPlayers().forEach((OfflinePlayer player) -> {
             whitelist.add(new Whitelist().offlinePlayer(player));
         });
         return whitelist;
     }
 
     @OpenApi(
-        path = "/v1/whitelist",
-        method = HttpMethod.GET,
-        summary = "Get the whitelist",
-        tags = {"Server"},
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(from = Whitelist.class, isArray = true))
-        }
+            path = "/v1/whitelist",
+            method = HttpMethod.GET,
+            summary = "Get the whitelist",
+            tags = {"Server"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(from = Whitelist.class, isArray = true))
+            }
     )
-    public static void whitelistGet(Context ctx){
-        if(!Bukkit.getServer().hasWhitelist()){
+    public static void whitelistGet(Context ctx) {
+        if (!Bukkit.getServer().hasWhitelist()) {
             // TODO: Handle Errors better
             ctx.json("error: The server has whitelist disabled");
             return;
@@ -250,32 +308,32 @@ public class ServerApi {
     }
 
     @OpenApi(
-        path = "/v1/whitelist",
-        method = HttpMethod.POST,
-        summary = "Update the whitelist",
-        description = "Possible responses are: `success`, `failed`, `Error: duplicate entry`, and `No whitelist`.",
-        tags = {"Server"},
-        formParams = {
-                @OpenApiFormParam(name = "uuid", type = String.class),
-                @OpenApiFormParam(name = "name", type = String.class)
-        },
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
-        }
+            path = "/v1/whitelist",
+            method = HttpMethod.POST,
+            summary = "Update the whitelist",
+            description = "Possible responses are: `success`, `failed`, `Error: duplicate entry`, and `No whitelist`.",
+            tags = {"Server"},
+            formParams = {
+                    @OpenApiFormParam(name = "uuid", type = String.class),
+                    @OpenApiFormParam(name = "name", type = String.class)
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
+            }
     )
-    public static void whitelistPost(Context ctx){
+    public static void whitelistPost(Context ctx) {
         //TODO: handle the event that no uuid is passed by ctx
         final org.bukkit.Server bukkitServer = Bukkit.getServer();
-        if(!bukkitServer.hasWhitelist()){
+        if (!bukkitServer.hasWhitelist()) {
             ctx.json("No whitelist");
             return;
         }
         final File directory = new File("./");
-        
+
         final Whitelist newEntry = new Whitelist().uuid(ctx.formParam("uuid")).name(ctx.formParam("name"));
         Set<Whitelist> whitelist = getWhitelist();
-        for( Whitelist player: whitelist){
-            if(player.equals(newEntry)){
+        for (Whitelist player : whitelist) {
+            if (player.equals(newEntry)) {
                 ctx.json("Error: duplicate entry");
                 return;
             }
@@ -290,26 +348,27 @@ public class ServerApi {
             whitelistFile.close();
             bukkitServer.reloadWhitelist();
             ctx.json("success");
-          } catch (final IOException e) {
+        } catch (final IOException e) {
             log.warning("An error occurred updating whitelist.");
             e.printStackTrace();
             ctx.json("failed");
-          }
-    }
-    @OpenApi(
-        path = "/v1/plugins",
-        method = HttpMethod.GET,
-        summary = "Get a list of installed plugins",
-        description = "Responds with an array of objects containing keys name and enabled.",
-        tags = {"Plugins"},
-        responses = {
-            @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
         }
+    }
+
+    @OpenApi(
+            path = "/v1/plugins",
+            method = HttpMethod.GET,
+            summary = "Get a list of installed plugins",
+            description = "Responds with an array of objects containing keys name and enabled.",
+            tags = {"Plugins"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json"))
+            }
     )
-    public static void listPlugins(Context ctx){
-        ArrayList<Plugin> pluginList = new ArrayList<Plugin>();
-        for(org.bukkit.plugin.Plugin plugin: Bukkit.getPluginManager().getPlugins()){
-            pluginList.add(new Plugin().name(plugin.getName()).enabled(plugin.isEnabled()));
+    public static void listPlugins(Context ctx) {
+        ArrayList<io.servertap.api.v1.models.Plugin> pluginList = new ArrayList<>();
+        for (org.bukkit.plugin.Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+            pluginList.add(new io.servertap.api.v1.models.Plugin().name(plugin.getName()).enabled(plugin.isEnabled()));
         }
         ctx.json(pluginList);
     }
