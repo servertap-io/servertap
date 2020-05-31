1340d648ac5b79f2dab44a451fc7e297546bedad
Hugo Hedlund <hugo.hedlund@outlook.com>
2020-05-31 17:55:42 +0200

    Added /economy endpoint (#32)
    The added GET /economy endpoint returns information about the servers economy plugin.

--- src/main/java/io/servertap/api/v1/EconomyApi.java
+++ src/main/java/io/servertap/api/v1/EconomyApi.java
@@ -1,8 +1,10 @@
 package io.servertap.api.v1;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import io.javalin.plugin.openapi.annotations.*;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
@@ -13,10 +15,44 @@ import io.servertap.PluginEntrypoint;
 
 import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class EconomyApi {
+
     private enum TransactionType {
         PAY, DEBIT
+    }
+
+    @OpenApi(
+            path = "/v1/economy",
+            method = HttpMethod.GET,
+            summary = "Economy plugin information",
+            tags = {"Economy"},
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(type = "application/json")),
+                    @OpenApiResponse(status = "500", content = @OpenApiContent(type = "application/json"))
+            }
+    )
+
+    public static void getEconomyPluginInformation(Context ctx){
+        Plugin econPlugin;
+        if (PluginEntrypoint.getEconomy() == null){
+            throw new InternalServerErrorResponse(Constants.VAULT_MISSING);
+        } else{
+            RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+            if (rsp == null) {
+                throw new InternalServerErrorResponse(Constants.ECONOMY_PLUGIN_MISSING);
+            }
+            econPlugin = rsp.getPlugin();
+        }
+
+        io.servertap.api.v1.models.Plugin plugin = new io.servertap.api.v1.models.Plugin();
+        plugin.setName(econPlugin.getName());
+        plugin.setEnabled(econPlugin.isEnabled());
+        plugin.setVersion(econPlugin.getDescription().getVersion());
+
+        ctx.json(plugin);
     }
 
     @OpenApi(
