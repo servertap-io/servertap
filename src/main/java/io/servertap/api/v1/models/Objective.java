d975c68de12a1392e1d90502713f2845556782f5
tcmaxwel <tcmaxwel@ncsu.edu>
2020-07-19 20:40:08 -0400

    Added a scoreboard endpoint

---
+++ src/main/java/io/servertap/api/v1/models/Objective.java
@@ -0,0 +1,62 @@
+package io.servertap.api.v1.models;
+
+import com.google.gson.annotations.Expose;
+
+import java.util.Set;
+
+public class Objective {
+	@Expose
+	private String name;
+
+	@Expose
+	private String displayName;
+
+	@Expose
+	private String displaySlot;
+
+	@Expose
+	private String criterion;
+
+	@Expose
+	private Set<Score> scores;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getCriterion() {
+		return criterion;
+	}
+
+	public void setCriterion(String criterion) {
+		this.criterion = criterion;
+	}
+
+	public Set<Score> getScores() {
+		return scores;
+	}
+
+	public void setScores(Set<Score> scores) {
+		this.scores = scores;
+	}
+
+	public String getDisplaySlot() {
+		return displaySlot;
+	}
+
+	public void setDisplaySlot(String displaySlot) {
+		this.displaySlot = displaySlot;
+	}
+}
---
+++ src/main/java/io/servertap/api/v1/models/Score.java
@@ -0,0 +1,27 @@
+package io.servertap.api.v1.models;
+
+import com.google.gson.annotations.Expose;
+
+public class Score {
+	@Expose
+	private String entry;
+
+	@Expose
+	private int value;
+
+	public String getEntry() {
+		return entry;
+	}
+
+	public void setEntry(String entry) {
+		this.entry = entry;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
