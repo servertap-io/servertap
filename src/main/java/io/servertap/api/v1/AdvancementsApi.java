7432804cb78f56cd228f5af48fb0bc5c2d165e33
Will Warren <willdavidwarren@gmail.com>
2023-04-11 09:15:46 -0400

    add missing OpenApi annotations for advancements

--- src/main/java/io/servertap/api/v1/AdvancementsApi.java
+++ src/main/java/io/servertap/api/v1/AdvancementsApi.java
@@ -1,13 +1,29 @@
 package io.servertap.api.v1;
 
 import io.javalin.http.Context;
+import io.javalin.openapi.OpenApi;
+import io.javalin.openapi.OpenApiContent;
+import io.javalin.openapi.OpenApiParam;
+import io.javalin.openapi.OpenApiResponse;
 import io.servertap.api.v1.models.Advancement;
+import io.servertap.api.v1.models.Player;
 import org.bukkit.Bukkit;
 
 import java.util.ArrayList;
 
 public class AdvancementsApi {
 
+    @OpenApi(
+            path = "/v1/advancements",
+            summary = "Gets all server advancements",
+            tags = {"Advancement"},
+            headers = {
+                    @OpenApiParam(name = "key")
+            },
+            responses = {
+                    @OpenApiResponse(status = "200", content = @OpenApiContent(from = Advancement.class))
+            }
+    )
     public static void getAdvancements(Context ctx) {
         final ArrayList<Advancement> advancements = new ArrayList<>();
 
